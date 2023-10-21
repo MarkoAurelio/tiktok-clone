b5b6d75bb7619ddc07e51388c11b8ce2afb8ff4c
Marko <madias.andrade@gmail.com>
2023-10-21 18:23:22 -0300

    feat(init): base project

---
+++ layouts/MainLayout.vue
@@ -0,0 +1,12 @@
+<template>
+    <TopNav />
+    <div :class="route.fullPath === '/' ? 'max-w-[1140px]' : ''" class="flex justify-between mx-auto w-full lg:px-2.5 px-0">
+        <div>
+        </div>
+        <slot />
+    </div>
+</template>
+
+<script setup>
+const route = useRoute()
+</script>
